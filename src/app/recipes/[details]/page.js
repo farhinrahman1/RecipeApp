ead2fe094421dc842108e588ec57b000af36c312
Farhin Rahman <farhin.rahmanp@gmail.com>
2024-10-14 22:40:55 +0600

    each items error

--- src/app/recipes/[details]/page.js
+++ src/app/recipes/[details]/page.js
@@ -1,10 +1,20 @@
+import RecipeDetailsItems from "@/components/recipes-details";
 
 
+async function fetchRecipeDetails(currentRecipeId) {
+    try {
+        const apiResponse = await fetch("https://dummyjson.com/recipes/${currentRecipeId}");
+        const data = await apiResponse.json();
+        return data;
+    } catch(error) {
+        throw new Error(error);        
+    }
+}
 
-export default async function RecipeDetails() {
+
+export default async function RecipeDetails({params}) {
+    const getRecipeDetails = await fetchRecipeDetails(params?.details);
     return (
-        <div>
-            <h1>Recipe Details</h1>
-        </div>
+        <RecipeDetailsItems getRecipeDetails={getRecipeDetails} />
     )
 }
