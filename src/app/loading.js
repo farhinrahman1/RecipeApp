507791982d091ea6e4c6427f82e795669f8b8db6
Farhin Rahman <farhin.rahmanp@gmail.com>
2024-10-14 21:56:55 +0600

    id fixed

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,8 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div>
-      <h1 className="text-black font-bold mb-6 text-2xl items-center flex">Loading...</h1>
+    <div className="text-black font-bold mb-6 text-2xl items-center flex">
+      <h1>Loading...</h1>
         <Skeleton className="h-4 w-1/2" />
     </div>
   );
--- src/components/ui/recipes-list/index.js
+++ src/components/ui/recipes-list/index.js
@@ -10,11 +10,13 @@ import Link from "next/link";
 
 
 export default function RecipeList({ recipes }) {
-    // console.log(recipes);
     return (
         <div>
             <div className="p-4 mx-auto lg:max-w-full md:max-w-4xl sm:max-w-full">
-                <h2 className="text-4xl font-bold text-gray-800 mb-12">Recipes</h2>
+                <div className="flex justify-between items-center mb-12">
+                     <h2 className="text-4xl font-bold text-gray-800">Recipes</h2>
+                    <Link href={"/"} className="mx-4 px-4 py-2 bg-gray-700 text-white rounded-lg shadow-md hover:bg-gray-600 transition-all text-lg font-medium">Go Home</Link>
+               </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                     {recipes && recipes.length > 0 ?
                         recipes.map(recipe =>(
